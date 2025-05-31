d1ff57547aca6e967f92ae1b904db09eff91170c
Sasha Vak <mak12121212@gmail.com>
2025-05-31 20:31:21 +0300

    create app

---
+++ src/components/FlashCard.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Flashcard() {
+    const [flipped, setFlipped] = useState(false)
+
+    return (
+        <div className="cards-box" onClick={() => setFlipped(!flipped)}>
+            <div className="cards-content">{flipped ? word.ru : word.en}</div>
+        </div>
+    )
+}
+export default Flashcard
